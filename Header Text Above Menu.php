5cac416dd5714c49b232217251fc56dfb66d1efb
Brett Redvers <dollhousedigital@gmail.com>
2016-09-20 20:30:49 +0100

    Create Header Text Above Menu.php

---
+++ Header Text Above Menu.php
@@ -0,0 +1,10 @@
+Add in header.php 
+below:
+<div class="logo_container">..........
+</div>
+
+and above:
+<div id="et-top-navigation" data-height="<?php echo esc_attr( et_get_option.........
+
+
+<div id="headertext"><h2><span style="color:#ffffff;">Mobile: <strong>01234 123456</strong><br/>Tel: <strong>01234 123456</strong><br/>Phone us now</span></h2></div>
